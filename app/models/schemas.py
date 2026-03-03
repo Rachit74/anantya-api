c5d392f32add429f678103ed81a28a75e943753b
rachit74 <rachithooda09@gmail.com>
2026-03-03 18:00:42 +0530

    update schema

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, HttpUrl
+from typing import List
 
 class OnboardingPost(BaseModel):
     email: EmailStr
@@ -9,6 +10,9 @@ class OnboardingPost(BaseModel):
     phone_number: str
     profession: str
     place_of_profession: str
-    department: str # change to list
+    department: List[str]
     volunteered_before: str
     acknowledgement: bool
+    can_attend_events: bool
+    government_id_picture: HttpUrl
+    member_picture: HttpUrl
